1f8cabe92ee8dad6965c7df6e45cb6d26338787b
Gabriel Henrique <souzatrindade28@gmail.com>
2021-02-19 20:43:25 -0300

    feat: insert user password with SHA256 and  authenticate with jwt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,19 @@ const jwt = require('jsonwebtoken');
 
 
 router.post('/', async (req, res)=>{
-    res.status(200).json({auth: true});
+    const result = await User.findAll({
+        where:{
+            userName: req.body.userName,
+            password: sha256("1%ti"+req.body.password+"@#ti-senac.$ARA")
+        }
+    });
+
+    if(!result.length){
+        res.status(401).json({auth: false});
+    }
+
+    const token = jwt.sign({id: result.id}, '@tiARA', {expiresIn: 600});
+    res.status(200).json({auth: true, token: token});
 });
 
 module.exports = router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const { User } = require('../models');
+const sha256 = require('js-sha256');
+const checkJWT = require('../middlewares/auth');
 
-router.get('/', async(req, res)=>{
-    
-    res.status(200).json([]);
+router.get('/', checkJWT, async(req, res)=>{
+    const users = await User.findAll();
+    res.status(200).json(users);
 });
 
 router.post('/', async(req, res)=>{
-    res.status(201).json([]);
+    const user = await User.create({
+        userName: req.body.userName,
+        password: sha256("1%ti"+req.body.password+"@#ti-senac.$ARA"),
+        email: req.body.email
+    });
+    res.status(201).json(user);
+});
+
+router.delete('/:id', async(req, res)=>{
+    const user = await User.destroy({
+        where:{
+            id: req.params.id
+        }
+    });
+    res.status(200).json(user);
 });
 
 module.exports = router
